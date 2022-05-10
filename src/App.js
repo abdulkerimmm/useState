84f11f1b69045e155d42c5ca8f7d9ebedd294bee
abdulkerim yalçın <64563946+abdulkerimmm@users.noreply.github.com>
2022-05-10 15:49:34 +0300

    Custom message

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const input = (type,placeholder,value,onchange) => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  )
+}
+
+export default input
