7ca6389c1820cd81384fc415da24db1d5080be38
abdulkerim yalçın <64563946+abdulkerimmm@users.noreply.github.com>
2024-01-21 23:27:08 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const input = (type,placeholder,value,onchange) => {
+export const App = () => {
+  const [name, setName] = useState('abdulkerim')
+  let withoutStateName = "abdulkerim";
+  console.log("1");
+  useEffect(() =>{
+    console.log('useEffect')
+  }, [])
+  console.log("2");
+
   return (
     <div>
-      <input type="text" />
+      {  console.log("3")}
+      
+      <button onClick={() =>{
+        {  console.log("4")}
+        setName('Salah')
+        withoutStateName="else"
+        {  console.log("5")}
+      }}>Change names</button>
+
+      <p>useState Value: {name}</p>
+      <p>without useState Value: {withoutStateName}</p>
+      {  console.log("6")}
     </div>
   )
 }
 
-export default input
+export default App
