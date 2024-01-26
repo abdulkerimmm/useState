b7092820c6d64c7737fe47587f3c8323941650a0
abdulkerim yalçın <64563946+abdulkerimmm@users.noreply.github.com>
2024-01-26 11:56:35 +0300

    added a console

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const App = () => {
-  const [name, setName] = useState('abdulkerim')
+  const [name, setName] = useState("abdulkerim");
   let withoutStateName = "abdulkerim";
   console.log("1");
-  useEffect(() =>{
-    console.log('useEffect')
-  }, [])
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
   console.log("2");
-
+  console.log("in am in the my first branch");
   return (
     <div>
-      {  console.log("3")}
-      
-      <button onClick={() =>{
-        {  console.log("4")}
-        setName('Salah')
-        withoutStateName="else"
-        {  console.log("5")}
-      }}>Change names</button>
+      {console.log("3")}
+
+      <button
+        onClick={() => {
+          {
+            console.log("4");
+          }
+          setName("Salah");
+          withoutStateName = "else";
+          {
+            console.log("5");
+          }
+        }}
+      >
+        Change names
+      </button>
 
       <p>useState Value: {name}</p>
       <p>without useState Value: {withoutStateName}</p>
-      {  console.log("6")}
+      {console.log("6")}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
